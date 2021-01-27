fb3c6a74cf1cc2780276c68334849b4a08b08108
praivesi <praivesi@gmail.com>
2021-01-28 08:21:22 +0900

    Initial commit

---
+++ src/test/java/com/tutorial/ohDiaraySpringBoot/OhDiaraySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.ohDiaraySpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhDiaraySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
