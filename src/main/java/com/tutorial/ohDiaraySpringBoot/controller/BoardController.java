a1435c37348bc65493ce8a17085978cd73803009
praivesi <praivesi@gmail.com>
2021-02-01 02:53:46 +0900

    Add form.html & Add Insert, Update button in form.html & Add validtion for user input in form.html

--- src/main/java/com/tutorial/ohDiaraySpringBoot/controller/BoardController.java
+++ src/main/java/com/tutorial/ohDiaraySpringBoot/controller/BoardController.java
@@ -2,12 +2,14 @@ package com.tutorial.ohDiaraySpringBoot.controller;
 
 import com.tutorial.ohDiaraySpringBoot.model.Board;
 import com.tutorial.ohDiaraySpringBoot.repository.BoardRepository;
+import com.tutorial.ohDiaraySpringBoot.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -17,6 +19,9 @@ public class BoardController {
     @Autowired
     private BoardRepository boardRepository;
 
+    @Autowired
+    private BoardValidator boardValidator;
+
     @GetMapping("/list")
     public String list(Model model) {
         List<Board> boards = boardRepository.findAll();
@@ -24,4 +29,47 @@ public class BoardController {
 
         return "board/list";
     }
+
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id){
+        if(id == null)
+        {
+            model.addAttribute("board", new Board());
+        }
+        else {
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+        }
+
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String formSubmit(@Valid Board board, BindingResult bindingResult){
+        boardValidator.validate(board, bindingResult);
+
+       if(bindingResult.hasErrors()) {
+           return "board/form";
+       }
+
+       boardRepository.save(board);
+
+       return "redirect:/board/list";
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/tutorial/ohDiaraySpringBoot/model/Board.java
+++ src/main/java/com/tutorial/ohDiaraySpringBoot/model/Board.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data
@@ -13,6 +15,9 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Size(min=2, max=30, message = "제목은 2글자 이상 30 글자 이하여야 합니다.")
     private String title;
     private String content;
 }
