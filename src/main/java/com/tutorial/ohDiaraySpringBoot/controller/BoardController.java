b588c0cdd7a4ee41d3609ce67a634958a37e7fcf
praivesi <praivesi@gmail.com>
2021-02-01 08:09:03 +0900

    Add search function and Paging in list.html
    Tutorial Link: https://www.youtube.com/watch?v=hmSPJHtZyp4

--- src/main/java/com/tutorial/ohDiaraySpringBoot/controller/BoardController.java
+++ src/main/java/com/tutorial/ohDiaraySpringBoot/controller/BoardController.java
@@ -4,6 +4,10 @@ import com.tutorial.ohDiaraySpringBoot.model.Board;
 import com.tutorial.ohDiaraySpringBoot.repository.BoardRepository;
 import com.tutorial.ohDiaraySpringBoot.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -23,8 +27,16 @@ public class BoardController {
     private BoardValidator boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Board> boards = boardRepository.findAll();
+    public String list(Model model,@PageableDefault(size = 2) Pageable pageable,
+                       @RequestParam(required = false, defaultValue = "") String searchText) {
+        //Page<Board> boards = boardRepository.findAll(pageable);
+        Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
+
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+        int endPage = Math.max(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("boards", boards);
 
         return "board/list";
