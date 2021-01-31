a682e5886456fa53d8aebf3e38060c20554c4989
praivesi <praivesi@gmail.com>
2021-02-01 07:13:07 +0900

    Add RESTful API with adding BoardApiController
    Tutorial Link: https://www.youtube.com/watch?v=XHT6S5JH_RU&t=8s

--- src/main/java/com/tutorial/ohDiaraySpringBoot/repository/BoardRepository.java
+++ src/main/java/com/tutorial/ohDiaraySpringBoot/repository/BoardRepository.java
@@ -3,6 +3,9 @@ package com.tutorial.ohDiaraySpringBoot.repository;
 import com.tutorial.ohDiaraySpringBoot.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BoardRepository extends JpaRepository<Board, Long> {
+import java.util.List;
 
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title, String content);
 }
