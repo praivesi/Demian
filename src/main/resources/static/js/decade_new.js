5b151b41d195f75054c7232fb65fec57bd4a0634
praivesi <praivesi@gmail.com>
2021-04-07 08:38:15 +0900

    Add edit-btn-container and edit-btn-item in decade_new.html

--- src/main/resources/static/js/decade_new.js
+++ src/main/resources/static/js/decade_new.js
@@ -1,6 +1,23 @@
 $(document).ready(function(){
-    $('.desire-container').css('margin-top', $('.job-nav-container').height() + 'px');
-    $('.desire-container').height($('.desire-container').height() - $('.job-nav-container').height());
+    console.log("BEFORE edit-btn-container height => " + $('.edit-btn-container').height());
+    console.log("job-nav-container height => " + $('.job-nav-container').height());
+
+    jobNavContainerHeight = $('.job-nav-container').height();
+    $('.edit-btn-container').height(jobNavContainerHeight);
+
+//    $('.edit-btn-container').height($('.job-nav-container').height() + 1);
+//    $('.edit-btn-container').css('height', $('job-nav-container').height() + 'px !important');
+    console.log("AFTER edit-btn-container height => " + $('.edit-btn-container').height());
+    console.log("job-nav-container height => " + $('.job-nav-container').height());
+
+//    console.log("BEFORE edit-btn-container-id height => " + $('#edit-btn-container-id').height());
+//    console.log("job-nav-container height => " + $('.job-nav-container').height());
+//    $('#edit-btn-container-id').height($('.job-nav-container').height());
+//    console.log("AFTER edit-btn-container-id height => " + $('#edit-btn-container-id').height());
+//    console.log("job-nav-container height => " + $('.job-nav-container').height());
+
+//    $('.desire-container').css('margin-top', $('.job-nav-container').height() + 'px');
+//    $('.desire-container').height($('.desire-container').height() - $('.job-nav-container').height());
 
     $('#decade-left-arrow').on('click', function(){
         var uriProtocol = window.location.protocol + '//' + window.location.host;
