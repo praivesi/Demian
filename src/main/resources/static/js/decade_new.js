d63699ec21bea62ba38e1fc75d3da8bda2bf36ca
praivesi <praivesi@gmail.com>
2021-04-06 21:41:16 +0900

    Synchronize layouts of desire-container and job-container

--- src/main/resources/static/js/decade_new.js
+++ src/main/resources/static/js/decade_new.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+    $('.desire-container').css('margin-top', $('.job-nav-container').height() + 'px');
+    $('.desire-container').height($('.desire-container').height() - $('.job-nav-container').height());
+
     $('#decade-left-arrow').on('click', function(){
         var uriProtocol = window.location.protocol + '//' + window.location.host;
         var uri = uriProtocol + "/schedule/decade_new/set_start_year/" + (startYear - 10);
@@ -7,9 +10,9 @@ $(document).ready(function(){
     });
 
     $('#decade-right-arrow').on('click', function(){
-            var uriProtocol = window.location.protocol + '//' + window.location.host;
-            var uri = uriProtocol + "/schedule/decade_new/set_start_year/" + (startYear + 10);
+        var uriProtocol = window.location.protocol + '//' + window.location.host;
+        var uri = uriProtocol + "/schedule/decade_new/set_start_year/" + (startYear + 10);
 
-            window.location.replace(uri);
-        });
+        window.location.replace(uri);
+    });
 });
