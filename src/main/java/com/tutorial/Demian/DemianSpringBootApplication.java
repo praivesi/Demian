dddac97ccd5f65d469b6d075ea814974a9fc6094
praivesi <praivesi@gmail.com>
2021-06-10 23:06:52 +0900

    feat: add SpringBootServletInitializer inheritance to DemianSpringBootApplication class

--- src/main/java/com/tutorial/Demian/DemianSpringBootApplication.java
+++ src/main/java/com/tutorial/Demian/DemianSpringBootApplication.java
@@ -2,9 +2,15 @@ package com.tutorial.Demian;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemianSpringBootApplication {
+public class DemianSpringBootApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(DemianSpringBootApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemianSpringBootApplication.class, args);
