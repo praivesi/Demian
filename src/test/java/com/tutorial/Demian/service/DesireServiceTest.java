93f01090d35c6092ea3417904b2a78acdfd5f0db
praivesi <praivesi@gmail.com>
2021-06-24 09:03:04 +0900

    test: comment DesireServiceTest code temporarily for AWS practice

--- src/test/java/com/tutorial/Demian/service/DesireServiceTest.java
+++ src/test/java/com/tutorial/Demian/service/DesireServiceTest.java
@@ -1,17 +1,17 @@
 package com.tutorial.Demian.service;
+//
+//import com.tutorial.Demian.dto.DesireDTO;
+//import com.tutorial.Demian.repository.DesireRepository;
+//import com.tutorial.Demian.repository.UserRepository;
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.transaction.annotation.Transactional;
 
-import com.tutorial.Demian.dto.DesireDTO;
-import com.tutorial.Demian.repository.DesireRepository;
-import com.tutorial.Demian.repository.UserRepository;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
-@SpringBootTest
-@Transactional
+//@SpringBootTest
+//@Transactional
 public class DesireServiceTest {
 
 //    private DesireService desireService;
@@ -22,18 +22,18 @@ public class DesireServiceTest {
 //    void setup(){
 //        this.desireService = new DesireService(this.desireRepository);
 //    }
-
-    @Test
-    void save_succeed(){
-//        // given
-//        DesireDTO desireDTO = new DesireDTO();
-//        desireDTO.setTitle("test title");
 //
-//        // when
-//        DesireDTO result = this.desireService.save(desireDTO);
-//
-//        // then
-//        Assertions.assertThat(result.getId()).isNotEqualTo(-1L);
-    }
+//    @Test
+//    void save_succeed(){
+////        // given
+////        DesireDTO desireDTO = new DesireDTO();
+////        desireDTO.setTitle("test title");
+////
+////        // when
+////        DesireDTO result = this.desireService.save(desireDTO);
+////
+////        // then
+////        Assertions.assertThat(result.getId()).isNotEqualTo(-1L);
+//    }
 }
 
