c5c70b03f4c9809c5d8e2240b72a9b5761470dd1
praivesi <praivesi@gmail.com>
2021-09-06 21:34:18 +0900

    refactor: restore unit test codes

--- src/test/java/com/tutorial/Demian/service/DesireServiceTest.java
+++ src/test/java/com/tutorial/Demian/service/DesireServiceTest.java
@@ -1,39 +1,39 @@
 package com.tutorial.Demian.service;
-//
-//import com.tutorial.Demian.dto.DesireDTO;
-//import com.tutorial.Demian.repository.DesireRepository;
-//import com.tutorial.Demian.repository.UserRepository;
-//import org.assertj.core.api.Assertions;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.transaction.annotation.Transactional;
 
-//@SpringBootTest
-//@Transactional
+import com.tutorial.Demian.dto.DesireDTO;
+import com.tutorial.Demian.repository.DesireRepository;
+import com.tutorial.Demian.repository.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
 public class DesireServiceTest {
 
-//    private DesireService desireService;
-//
-//    @Autowired private DesireRepository desireRepository;
-//
-//    @BeforeEach
-//    void setup(){
-//        this.desireService = new DesireService(this.desireRepository);
-//    }
-//
-//    @Test
-//    void save_succeed(){
-////        // given
-////        DesireDTO desireDTO = new DesireDTO();
-////        desireDTO.setTitle("test title");
-////
-////        // when
-////        DesireDTO result = this.desireService.save(desireDTO);
-////
-////        // then
-////        Assertions.assertThat(result.getId()).isNotEqualTo(-1L);
-//    }
+    private DesireService desireService;
+
+    @Autowired private DesireRepository desireRepository;
+
+    @BeforeEach
+    void setup(){
+        this.desireService = new DesireService(this.desireRepository);
+    }
+
+    @Test
+    void save_succeed(){
+        // given
+        DesireDTO desireDTO = new DesireDTO();
+        desireDTO.setTitle("test title");
+
+        // when
+        DesireDTO result = this.desireService.save(desireDTO);
+
+        // then
+        Assertions.assertThat(result.getId()).isNotEqualTo(-1L);
+    }
 }
 
